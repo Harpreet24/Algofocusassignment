a0de0e6758a06e6c297bbf741ec871e9d341cf92
Harpreet24 <58454611+Harpreet24@users.noreply.github.com>
2020-10-30 19:05:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,71 @@
+import os
+import time
+
+from flask import Flask, render_template, request
+
+#from ocr import ocr_core
+import pytesseract
+
+#import Image
+
+
+UPLOAD_FOLDER = '/static/uploads/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+def ocr_core(filename):
+    """
+    This function will handle the core OCR processing of images.
+    """
+    pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+    text = pytesseract.image_to_string(Image.open(filename))# We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
+
+    return text
+
+print(ocr_core('Car_Image_1.jpg'))
+
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_page():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return render_template('upload.html', msg='No file selected')
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            return render_template('upload.html', msg='No file selected')
+
+        if file and allowed_file(file.filename):
+            file.save(os.path.join(os.getcwd() + UPLOAD_FOLDER, file.filename))
+
+            # call the OCR function on it
+            extracted_text = ocr_core(file)
+
+            # extract the text and display it
+            return render_template('upload.html',
+                                   msg='Successfully processed',
+                                   extracted_text=extracted_text,
+                                   img_src=UPLOAD_FOLDER + file.filename)
+    elif request.method == 'GET':
+        return render_template('upload.html')
+
+if __name__ == '__main__':
+    app.run()
