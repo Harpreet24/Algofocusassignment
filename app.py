7b9174c5d66c9259140b202aefccac6df57f45c4
Harpreet24 <58454611+Harpreet24@users.noreply.github.com>
2020-10-31 08:47:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,16 +24,16 @@ try:
     from PIL import Image
 except ImportError:
     import Image
-def ocr_core(filename):
+def ocr(filename):
     """
     This function will handle the core OCR processing of images.
     """
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
     text = pytesseract.image_to_string(Image.open(filename))# We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
 
     return text
 
-print(ocr_core('Car_Image_1.jpg'))
+print(ocr('Car_Image_1.jpg'))
 
 
 @app.route('/')
@@ -57,7 +57,7 @@ def upload_page():
             file.save(os.path.join(os.getcwd() + UPLOAD_FOLDER, file.filename))
 
             # call the OCR function on it
-            extracted_text = ocr_core(file)
+            extracted_text = ocr(file)
 
             # extract the text and display it
             return render_template('upload.html',
