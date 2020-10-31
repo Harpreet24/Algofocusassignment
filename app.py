f537967e0b096f92afc4fbf2075726d641c15e84
Harpreet24 <58454611+Harpreet24@users.noreply.github.com>
2020-10-31 09:23:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def ocr(filename):
     """
     This function will handle the core OCR processing of images.
     """
-    pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
+    pytesseract.pytesseract.tesseract_cmd = r'./.apt/usr/share/tesseract-ocr/4.00/tessdata'
     text = pytesseract.image_to_string(Image.open(filename))# We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
 
     return text
